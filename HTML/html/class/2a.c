13d2f2ba91515a0cac36a4aef8383620a7377c0b
mantu <mantukumar87586299@gmail.com>
2023-09-07 20:14:30 +0530

    done

---
+++ HTML/html/class/2a.c
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title> 
+</head> 
+<body> 
+    <h1 style="color:red"><b><i><u>OOP USING C++</u></i></b></h1> 
+    <h2> yashvant kanetkar</h2> 
+   <p style="color:blue"> This book covers C++ starting from basic level and moving on to cover advanced level features. It requires no prior knowledge of C or any other programming</p> 
+   <p> 
+
+         <h1 style="color:red"><b><i><u>Let Us c</u></i></b></h1> 
+    <h2> yashvant kanetkar</h2> 
+   <p style="color:blue"> This book is covers all basics oof f and give fundamental of programing</p> 
+   <p> 
+
+
+   <h1 style="color:red"><b><i><u>Core Java An Integrated Approach (Black Book)</u></i></b></h1> 
+    <h2>Dr. R. Nageswara Rao</h2> 
+   <p style="color:blue">The book is written in such a way that learners without any background in programming are able to follow and understand it entirely. It discusses the concepts of Java in a simple and straightforward language with a clear cut explanation, without beating around the bush. The book provides ample solved programs which could be used by the students not only in their examinations but also to remove the fear of programming from their minds. It also covers sample interview questions which were asked in various interviews. It helps students to prepare for their future careers.</p> 
+   <p> 
+
+        <em> 
+         MANTU KUMAR MORYA<br> 
+         92201703001<br> 
+      </em> 
+    </p> 
+     
+</body> 
+</html>
